844a76693501e2368cdac4480ebda7107cf8d36d
leakagegh <thehealer184@gmail.com>
2023-10-29 16:08:11 -0700

    COMPLTE

---
+++ 0-bubble_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order
+ *               using the bubble sort algorithm.
+ * @array: Pointer to the array to be sorted.
+ * @size: The number of elements in the array.
+ *
+ * Description: This function implements the bubble sort algorithm
+ *              to sort an array of integers in ascending order.
+ *              It compares adjacent elements and swaps them if they
+ *              are in the wrong order until the entire array is sorted.
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j + 1] < array[j])
+			{
+				/* Swap adjacent elements */
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				/* Print the current state of the array */
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ Authors_Contributors.md
@@ -0,0 +1,1 @@
+Mohammed Awal Osman "LeakageGH"
